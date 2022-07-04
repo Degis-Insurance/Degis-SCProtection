5eeb9a184fc5972d4f73971e4c6b630b1076ea3b
liyang-WSL <ylikp.ust@gmail.com>
2022-07-04 14:12:31 +0800

    :bug: Change solhint config and fix typo

--- src/voting/VotingCenter.sol
+++ src/voting/VotingCenter.sol
@@ -20,17 +20,16 @@
 
 pragma solidity ^0.8.13;
 
-import "../mock/mockVeDEG.sol";
+import "../mock/MockVeDEG.sol";
 
 contract VotingCenter {
-
     uint256[3] voteWeight;
     address veDEG;
     uint256 public totalveDEG;
 
     struct Quorum {
-       uint256 totalVotes;
-       uint256 weightedYesVotes;
+        uint256 totalVotes;
+        uint256 weightedYesVotes;
     }
     // reportId => quorum
     mapping(uint256 => Quorum) quorum;
@@ -38,9 +37,13 @@ contract VotingCenter {
     function setVoteWeight(uint256[3] _voteWeight) external onlyOwner {}
 
     function submitVote(uint256 _reportId, bool _vote) external {}
+
     function submitComitteeVote(uint256 _reportId, bool _vote) external {}
+
     function submitTeamVote(uint256 _reportId, bool _vote) external {}
+
     function closeVote(uint256 _reportId) external {}
+
     function countAndFowardVote(uint256 _reportId) external {}
 
     // ---------------------------------------------------------------------------------------- //
