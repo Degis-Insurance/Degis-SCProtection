6735394406d3c17590e1cb65a4b45bb031551aa3
SnowPrimate <lucasvinzon@gmail.com>
2022-07-31 12:47:27 -0300

    track changes

---
+++ src/voting/interfaces/IncidentReportParameters.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.13;
+
+import "./VotingResultParameters.sol";
+
+abstract contract IncidentReportParameters is VotingResultParameters {
+    // Status parameters for a report
+    uint256 constant INIT_STATUS = 0;
+    uint256 constant PENDING_STATUS = 1;
+    uint256 constant VOTING_STATUS = 2;
+    uint256 constant SETTLED_STATUS = 3;
+    uint256 constant CLOSE_STATUS = 404;
+
+    // Cool down time parameter
+    // If you submitted a wrong report, you cannot start another within cooldown period
+    uint256 public constant COOLDOWN_WRONGREPORT = 7 days;
+
+    // Voting time length parameters
+    uint256 constant PENDING_PERIOD = 3 days;
+    uint256 constant VOTING_PERIOD = 3 days;
+    uint256 constant EXTEND_PERIOD = 1 days;
+    uint256 constant SAMPLE_PERIOD = 1 days;
+
+    // Quorum parameter
+    uint256 constant QUORUM_RATIO = 30;
+
+    // DEG threshold for starting a report
+    uint256 constant REPORT_THRESHOLD = 1000 ether;
+
+    // DEG reward for correct reporter
+    uint256 constant REPORTER_REWARD = 1000 ether;
+
+    // Punishment for those who vote wrong
+    uint256 constant PUNISHMENT_RATIO = 40; // 40% go to winners, 40% reserve
+    uint256 constant DEBT_RATIO = 80; // 80% as the debt to unlock veDEG
+
+    uint256 constant SCALE = 1e12;
+}
---
+++ test/utils/BaseTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+
+/**
+ * @notice Some helper functions for running test in Solidity
+ */
+contract BaseTest is Test {
+    function mkaddr(string calldata name) public returns (address) {
+        address addr = address(
+            uint160(uint256(keccak256(abi.encodePacked(name))))
+        );
+
+        vm.label(addr, name);
+        return addr;
+    }
+}
