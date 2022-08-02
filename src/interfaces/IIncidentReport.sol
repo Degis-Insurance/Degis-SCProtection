b9467be47f24f9b80ff394c15a8c07369dd978fb
SnowPrimate <lucasvinzon@gmail.com>
2022-08-02 01:01:58 -0300

    fix: add compiler to incident report interface

--- src/interfaces/IIncidentReport.sol
+++ src/interfaces/IIncidentReport.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+pragma solidity ^0.8.13;
+
 interface IIncidentReport {
     event DebtPaid(address payer, address user, uint256 debt, uint256 unlockAmount);
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
