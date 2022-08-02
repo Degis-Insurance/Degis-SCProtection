be3543145c484f6e6935073a9fc17ebe8a039bcc
liyang-WSL <ylikp.ust@gmail.com>
2022-08-02 14:10:54 +0800

    feat: add deploy scripts

---
+++ deploy/0_deploy_proxyAdmin.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction, ProxyOptions } from "hardhat-deploy/types";
+
+import { readAddressList, storeAddressList } from "../scripts/contractAddress";
+
+// Deploy Proxy Admin
+// It is a non-proxy deployment
+// Contract:
+//    - ProxyAdmin
+// Tags:
+//    - ProxyAdmin
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy } = deployments;
+
+  network.name = network.name == "hardhat" ? "localhost" : network.name;
+
+  const { deployer } = await getNamedAccounts();
+
+  // Read address list from local file
+  const addressList = readAddressList();
+
+  // Proxy Admin contract artifact
+  const proxyAdmin = await deploy("ProxyAdmin", {
+    contract: "ProxyAdmin",
+    from: deployer,
+    args: [],
+    log: true,
+  });
+  addressList[network.name].ProxyAdmin = proxyAdmin.address;
+
+  // Store the address list after deployment
+  storeAddressList(addressList);
+};
+
+func.tags = ["ProxyAdmin"];
+export default func;
