0ee9dc139e762eec642f4954b8d70d5d9bd4cb6c
liyang-WSL <ylikp.ust@gmail.com>
2022-08-12 10:48:20 +0800

    feat: add cr token factory

--- src/crTokens/CoverRightToken.sol
+++ src/crTokens/CoverRightToken.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoverRightToken is ERC20 {
-    
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {}
