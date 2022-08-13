1ea8c9f7eee341204900fe975970afdac298a490
liyang-WSL <ylikp.ust@gmail.com>
2022-08-13 21:37:11 +0800

    feat: refactory incident report path

---
+++ src/voting/incidentReport/IncidentReportErrorEvent.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.13;
+
+interface IncidentReportEventError {
+    // ---------------------------------------------------------------------------------------- //
+    // *************************************** Events ***************************************** //
+    // ---------------------------------------------------------------------------------------- //
+
+    event ReportCreated(
+        uint256 reportId,
+        uint256 indexed poolId,
+        uint256 reportTimestamp,
+        address indexed reporter
+    );
+
+    event VotingStart(uint256 reportId, uint256 startTimestamp);
+
+    event ReportClosed(uint256 reportId, uint256 closeTimestamp);
+
+    event ReportVoted(
+        uint256 reportId,
+        address indexed user,
+        uint256 voteFor,
+        uint256 amount
+    );
+
+    event ReportSettled(uint256 reportId, uint256 result);
+
+    event ReportExtended(uint256 reportId, uint256 round);
+
+    event DebtPaid(
+        address payer,
+        address user,
+        uint256 debt,
+        uint256 unlockAmount
+    );
+
+    // ---------------------------------------------------------------------------------------- //
+    // *************************************** Errors ***************************************** //
+    // ---------------------------------------------------------------------------------------- //
+}
