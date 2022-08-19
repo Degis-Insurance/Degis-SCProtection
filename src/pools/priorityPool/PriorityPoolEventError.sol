ec87811befc0975c671ae621be88626713b1ec28
liyang-WSL <ylikp.ust@gmail.com>
2022-08-20 00:59:39 +0800

    feat: change price index mapping

--- src/pools/priorityPool/PriorityPoolEventError.sol
+++ src/pools/priorityPool/PriorityPoolEventError.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.13;
 interface PriorityPoolEventError {
     event LiquidityProvision(uint256 amount, address sender);
     event LiquidityRemoved(uint256 amount, address sender);
-    event Liquidation(uint256 amount, uint256 endDate);
+    event Liquidation(uint256 amount);
     event EmissionRateUpdated(
         uint256 newEmissionRate,
         uint256 newEmissionEndTime
