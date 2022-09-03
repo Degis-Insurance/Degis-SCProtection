92e5371c86cc83b17da62c24b7de57b1c02723d8
liyang-WSL <ylikp.ust@gmail.com>
2022-09-04 00:40:51 +0800

    fix: update IProtectionPool

--- src/interfaces/IPremiumRewardPool.sol
+++ src/interfaces/IPremiumRewardPool.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.13;
 
 interface IPremiumRewardPool {
-    function distributeShield(uint256 _amount) external;
-
     function distributeToken(address _token, uint256 _amount) external;
 
     function register(address _pool, address _token) external;
--- src/interfaces/IProtectionPool.sol
+++ src/interfaces/IProtectionPool.sol
@@ -3,65 +3,13 @@
 pragma solidity ^0.8.13;
 
 interface IProtectionPool {
-    function deg() external view returns (address);
-
-    function executor() external view returns (address);
-
-    function incidentReport() external view returns (address);
-
-    function priorityPoolFactory() external view returns (address);
-
-    function lastRewardTimestamp() external view returns (uint256);
-
-    function maxCapacity() external view returns (uint256);
-
-    function moveLiquidity(uint256 _poolId, uint256 _amount) external;
-
-    function onboardProposal() external view returns (address);
-
     function pauseProtectionPool(bool _paused) external;
-
-    function policyCenter() external view returns (address);
-
-    function poolInfo()
-        external
-        view
-        returns (
-            bool,
-            uint256,
-            uint256,
-            uint256,
-            uint256,
-            uint256
-        );
 
     function providedLiquidity(uint256 _amount, address _provider) external;
 
     function removedLiquidity(uint256 _amount, address _provider)
         external
         returns (uint256);
-
-    function setExecutor(address _executor) external;
-
-    function setIncidentReport(address _incidentReport) external;
-
-    function setPriorityPoolFactory(address _priorityPoolFactory) external;
-
-    function setOnboardProposal(address _onboardProposal) external;
-
-    function setPolicyCenter(address _policyCenter) external;
-
-    function setProtectionPool(address _protectionPool) external;
-
-    function shield() external view returns (address);
-
-    function startTime() external view returns (uint256);
-
-    function totalSupply() external view returns (uint256);
-
-    function updateRewards() external;
-
-    function veDeg() external view returns (address);
 
     function getTotalCovered() external view returns (uint256);
 
